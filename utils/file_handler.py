355c6b8d2d149945d2159723f2788c1687c61b7a
Seunga Kim <70243358+sinya3558@users.noreply.github.com>
2025-10-06 03:25:55 +0900

    fix lang

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QFileDialog
 import base64
 
 def get_image_file():
-    path, _ = QFileDialog.getOpenFileName(None, '이미지 선택', '', 'Images (*.png *.jpg *.jpeg)')
+    path, _ = QFileDialog.getOpenFileName(None, 'Choose Image', '', 'Images (*.png *.jpg *.jpeg)')
     return path
 
 
